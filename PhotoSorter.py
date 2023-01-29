c0aa27aaefdfe9d2e0ba91a7cf29b2204c30093e
debugggger <bulatovdima1@gmail.com>
2023-01-29 11:36:17 +0300

    Initial commit

---
+++ PhotoSorter.py
@@ -0,0 +1,145 @@
+import tkinter
+from enum import Enum
+from tkinter import *
+
+from sorter import Sorter
+import tkinter.messagebox as mb
+
+def browsePatch():
+    sorter.browsePatch()
+
+def browsePatchToExit():
+    sorter.browsePatchToExit()
+
+def startSort():
+    sorter.startSort()
+
+def setIsDir():
+    sorter.setIsDir(var.get())
+
+
+class App(Frame):
+
+    def __init__(self):
+        super().__init__()
+        global sorter
+        self.initUI()
+        sorter = Sorter(self, self.entryPatch, self.entryPatchToExit, self.lblProcess, self.btnStart, self.troubleFrame, self.troublesList)
+        self.lblPatch = Label
+        self.lblProcess = Label
+        self.btnStart = Button
+        self.entryPatch = Entry
+        self.entryPatchToExit = Entry
+        self.boxUseDirs = Checkbutton
+        self.troublesList = Text
+        self.troubleFrame = Frame
+        # self.entryDate1 = Entry
+        # self.entryDate2 = Entry
+
+    def initUI(self):
+        self.pack(fill=BOTH, expand=True)
+
+        patchesFrame = Frame(self, pady=5)
+        patchesFrame.pack(side=TOP, fill=X)
+
+        patchToExitFrame = Frame(patchesFrame, pady=5)
+        patchToExitFrame.pack(side=TOP, fill=X)
+        self.lblPatchToExit = Label(patchToExitFrame, text="отсортировать в:", width=15)
+        self.lblPatchToExit.pack(side=LEFT, padx=5, pady=5)
+        self.entryPatchToExit = Entry(patchToExitFrame)
+        self.entryPatchToExit.pack(side=LEFT, fill=X, padx=5, expand=True)
+        btnPatchToExitFind = Button(patchToExitFrame, cursor="hand2", text="поиск", command=browsePatchToExit)
+        btnPatchToExitFind.pack(side=LEFT, padx=5)
+
+        patchFrame = Frame(patchesFrame)
+        patchFrame.pack(side=TOP, fill=X)
+        self.lblPatch = Label(patchFrame, text="каталог с фото:", width=15)
+        self.lblPatch.pack(side=LEFT, padx=5, pady=5)
+        self.entryPatch = Entry(patchFrame)
+        self.entryPatch.pack(side=LEFT, fill=X, padx=5, expand=True)
+        btnPatchFind = Button(patchFrame, cursor="hand2", text="поиск", command=browsePatch)
+        btnPatchFind.pack(side=LEFT, padx=5)
+
+        dirFrame = Frame(self)
+        dirFrame.pack(side=TOP, fill=X)
+        global var
+        var = IntVar()
+        var.set(0)
+        self.boxUseDirs = Checkbutton(dirFrame, text="просмотр внутренних папок", variable=var,
+                 onvalue=1, offvalue=0,
+                 command=setIsDir)
+        self.boxUseDirs.pack(side=LEFT, padx=5, pady=5)
+
+        # dateFrame = Frame(self)
+        # dateFrame.pack(side=TOP, fill=X)
+        # lblDate1 = Label(dateFrame, text="дата с:", width=8)
+        # lblDate1.pack(side=LEFT, padx=5, pady=5)
+        # self.entryDate1 = Entry(dateFrame)
+        # self.entryDate1.pack(side=LEFT, fill=X, padx=5, expand=True)
+        # lblDate2 = Label(dateFrame, text="по:", width=2)
+        # lblDate2.pack(side=LEFT, padx=5, pady=5)
+        # self.entryDate2 = Entry(dateFrame)
+        # self.entryDate2.pack(side=LEFT, fill=X, padx=5, expand=True)
+
+
+        self.troubleFrame = Frame(self)
+        self.troubleFrame.pack(side=TOP, fill=X)
+        # lblTroubles = Label(self.troubleFrame, text="не удалось обработать", width=30)
+        # lblTroubles.pack(side=TOP, padx=5, pady=5)
+
+        listFrame = Frame(self.troubleFrame)
+        listFrame.pack(side=TOP, fill=X)
+
+        scrollbar = Scrollbar(listFrame, orient="vertical")
+        scrollbar.pack(side=RIGHT, fill=Y)
+        self.troublesList = Text(listFrame, width=20, height=4, yscrollcommand=scrollbar.set)
+        self.troublesList.pack(anchor=CENTER, padx=5, pady=5, fill=X)
+        scrollbar.config(command=self.troublesList.yview)
+
+        #self.troubleFrame.pack_forget()
+
+        startFrame = Frame(self)
+        startFrame.pack(anchor=S,  expand=True, pady=20)
+        self.btnStart = Button(startFrame, cursor="hand2", text="начать", command=startSort)
+        self.btnStart.pack(side=LEFT, fill=X, padx=5)
+        self.lblProcess = Label(startFrame, text="идет обработка", width=15)
+        self.lblProcess.pack(side=LEFT, padx=5)
+        self.lblProcess.pack_forget()
+
+
+    class Message(Enum):
+        ERROR = 1
+        WARNING = 2
+        INFO = 3
+        ASK_START = 4
+
+    def messageWindow(self, type, msg):
+        answ = 0
+        match type:
+            case self.Message.WARNING: mb.showwarning("предупреждение", msg)
+            case self.Message.INFO: mb.showinfo("инофрмация", msg)
+            case self.Message.ERROR: mb.showerror("ошибка", msg)
+            case self.Message.ASK_START: answ = mb.askokcancel("путь не задан", msg)
+
+        if answ == 1:
+            browsePatch()
+
+
+def main():
+    root = Tk()
+    app = App()
+    width = app.winfo_screenwidth()
+    height = app.winfo_screenheight()
+    x = (width / 2) - (width / 6)
+    y = (height / 2) - (height / 8)
+    root.geometry('%dx%d+%d+%d' % (width / 4, height / 3, x, y))
+    root.minsize(int(width / 4), int(height / 3))
+    root.maxsize(int(width / 2), int(height / 3))
+    root.iconphoto(True, tkinter.PhotoImage(file='./icon.png'))
+    root.title("Сортировщик фото")
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ sorter.py
@@ -0,0 +1,164 @@
+import os
+import shutil
+import threading
+from datetime import date, datetime
+from tkinter import filedialog, END
+
+import datetime
+
+from exif import Image
+from threading import Thread
+
+class Sorter(object):
+    def __init__(self, app, entryPatch, entryPatchToExit, procLabel, btnStart, troubleFrame, troublesList):
+        self.patch = ''
+        self.PatchToExit = ''
+        self.countImages = 0
+        self.app = app
+        self.entryPatch = entryPatch
+        self.entryPatchToExit = entryPatchToExit
+        self.procLbl = procLabel
+        self.stopThread = threading.Event()
+        self.btnStart = btnStart
+        self.troubleFrame = troubleFrame
+        self.troublesList = troublesList
+        self.isDir = 0
+        self.fineshedCount = 0
+        self.troubleFiles = []
+        self.errorCount = 0
+
+    def setIsDir(self, var):
+        self.isDir = var
+
+    def browsePatch(self):
+        self.entryPatch.delete(0, END)
+        self.patch = filedialog.askdirectory(initialdir="/", title="Выбор папки")
+        self.entryPatch.insert(0, self.patch)
+        self.troubleFiles = []
+        self.findImages()
+
+    def browsePatchToExit(self):
+        self.entryPatchToExit.delete(0, END)
+        self.PatchToExit = filedialog.askdirectory(initialdir="/", title="Выбор папки")
+        self.entryPatchToExit.insert(0, self.PatchToExit)
+        self.troubleFiles = []
+        for i in range(len(self.troubleFiles)):
+            self.troublesList.insert("end", self.troubleFiles[i])
+        self.troubleFrame.pack()
+
+    def checkDirs(self, path, mode, **kwargs):
+        month_list = kwargs.get('month_list', None)
+        for file in os.listdir(path):
+            current = path + "/" + file
+            if os.path.isdir(current):
+                self.checkDirs(current, mode, month_list=month_list)
+            if current.split('.')[-1] == 'jpg' or current.split('.')[-1] == 'JPG' or current.split('.')[-1] == 'png':
+                if mode == "find":
+                    self.countImages += 1
+                if mode == "work":
+                    self.fineshedCount += 1
+                    with open(current, "rb") as current:
+                        try:
+                            self.readDataFromImage(Image(current), file, path, month_list)
+                        except:
+                            self.errorCount += 1
+                            self.troubleFiles.append(path + "/" + file+"\n")
+
+    def findImages(self):
+        if self.patch == '':
+            return
+
+        self.countImages = 0
+        for file in os.listdir(self.patch):
+            current = self.patch+"/"+file
+            if os.path.isdir(current) and self.isDir == 1:
+                self.checkDirs(current, "find")
+
+            if current.split('.')[-1] == 'jpg' or current.split('.')[-1] == 'JPG' or current.split('.')[-1] == 'png':
+                self.countImages += 1
+
+        if self.countImages == 0:
+            self.app.messageWindow(self.app.Message.WARNING, "фото не найдены")
+        # else:
+        #     self.app.messageWindow(self.app.Message.INFO, "найдено " + str(self.countImages) + " фото")
+
+    def startSort(self):
+        threadSorting = Thread(target=self.sortByData)
+        threadSorting.start()
+
+    def sortByData(self):
+
+        # if self.date1 != '':
+        #     try:
+        #         datetime.datetime.strptime(str(self.date1), '%Y-%m-%d')
+        #     except:
+        #         self.app.messageWindow(self.app.Message.WARNING, "Укажите дату в формате YYYY-MM-DD")
+        #         return
+
+        self.findImages()
+        if self.countImages == 0:
+            return
+
+        self.troubleFiles = []
+        self.troublesList.delete(1.0, END)
+
+        self.errorCount = 0
+        if self.patch == '':
+            self.app.messageWindow(self.app.Message.ASK_START, "не указан путь для поиска файлов\n\nуказать?")
+            return
+
+        if self.PatchToExit == '':
+            self.PatchToExit = self.patch
+            self.entryPatchToExit.insert(0, self.PatchToExit)
+
+
+
+        self.procLbl.pack()
+        self.btnStart.pack_forget()
+
+        month_list = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь',
+                        'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
+
+        for file in os.listdir(self.patch):
+
+            current = self.patch + "/" + file
+            if os.path.isdir(current) and self.isDir == 1:
+                self.checkDirs(current, "work", month_list=month_list)
+
+
+            if file.split('.')[-1] == 'jpg' or file.split('.')[-1] == 'JPG' or file.split('.')[-1] == 'png':
+                try:
+                    with open(current, "rb") as current:
+                        self.readDataFromImage(Image(current), file, self.patch, month_list)
+                except:
+                    self.errorCount+=1
+                    self.troubleFiles.append(self.patch + "/" + file+"\n")
+            self.fineshedCount += 1
+        if self.errorCount != 0:
+            self.app.messageWindow(self.app.Message.ERROR, "не удалось обработать " + str(self.errorCount) + " фото")
+            self.troublesList.insert("end", "не удалось обработать:\n")
+            for i in range(len(self.troubleFiles)):
+                self.troublesList.insert("end", self.troubleFiles[i])
+            self.troubleFrame.pack()
+        else:
+            self.app.messageWindow(self.app.Message.INFO, "обработка успешна завершена")
+        self.procLbl.pack_forget()
+        self.btnStart.pack()
+        self.stopThread.set()
+
+    def readDataFromImage(self, image, file, patch, month_list=[]):
+        if image.has_exif:
+            data = str(image.datetime_original).split(':')
+            year = data[0]
+            month = data[1]
+
+            exitPatch = self.PatchToExit + "/" + year + "/" + month_list[int(month) - 1]
+            if not os.path.exists(exitPatch):
+                os.makedirs(exitPatch)
+            shutil.copyfile(patch + "/" + file, exitPatch + "/" + file)
+
+            percent = int(self.fineshedCount / self.countImages * 100)
+            if percent > 100:
+                percent = 100
+            self.procLbl.configure(text="обработано " + str(percent) + "%")
+
